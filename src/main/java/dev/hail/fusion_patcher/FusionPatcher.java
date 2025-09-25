5ecc4574e7d7f5c8a415dcfc6c3e82b4d058ab15
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-09-25 16:48:31 +0800

    Main class clean

--- src/main/java/dev/hail/fusion_patcher/FusionPatcher.java
+++ src/main/java/dev/hail/fusion_patcher/FusionPatcher.java
@@ -1,38 +1,8 @@
 package dev.hail.fusion_patcher;
 
-import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(FusionPatcher.MODID)
 public class FusionPatcher {
     public static final String MODID = "fusion_patcher";
-    public static final Logger LOGGER = LogUtils.getLogger();
-    public FusionPatcher(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
-        modEventBus.addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
-
-    @Mod.EventBusSubscriber(modid = FusionPatcher.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    static class ClientModEvents {
-        @SubscribeEvent
-        static void onClientSetup(FMLClientSetupEvent event) {
-        }
-    }
 }
